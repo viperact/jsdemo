6b7bed7a00075c87b6bf237dca54c4745e3f6260
lsh <viperact26@gmail.com>
2022-09-21 18:13:30 +0900

    update

---
+++ js024_문제풀이.js
@@ -0,0 +1,43 @@
+/*
+ 문제1
+ <프로그램 실행결과>
+  주어진 배열에서 3의 배수의 개수=> 6
+  주어진 배열에서 3의 배수의 합=> 108
+  */
+
+let arr = [1, 3, 5, 8, 9, 11, 15, 19, 18, 20, 30, 33, 31];
+
+//주어진 arr배열에서 3의 배수를 구해서 리턴하는 함수 정의
+function process(arr) {
+  return null;
+}
+
+//주어진 arr배열에서 3의 배수의 갯수를 리턴하는  함수 정의
+function count(process) {
+  return 0;
+}
+
+//주어진 arr배열에서 3의 배수 합을 리턴하는 함수 정의
+function arrPlus(process) {
+  return 0;
+}
+
+/*
+[문제2]
+[출력결과]
+javascript
+tpircsavaj
+javascript
+*/
+
+let alpa = ["j", "a", "v", "a", "s", "c", "r", "i", "p", "t"];
+
+//alpa배열의 요소를  reverse한 후 리턴하는 함수 정의
+function reverse(alpa) {
+  return null;
+}
+
+//data배열의 출력하는  함수 정의
+function display(data) {
+  return null;
+}
